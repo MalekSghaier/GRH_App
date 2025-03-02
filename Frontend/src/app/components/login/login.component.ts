2c78eae433227273ace24dfddf9870bf2db309d7
MalekSghaier <melak.seghaier@gmail.com>
2025-03-02 09:38:18 +0100

    Commit

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,13 +1,14 @@
-//login.component.ts
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr'; // Importation de Toastr
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -17,9 +18,10 @@ export class LoginComponent {
   errorMessage: string = '';
 
   constructor(
-    private fb: FormBuilder, 
-    private router: Router, 
-    private http: HttpClient
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient,
+    private toastr: ToastrService // Injection de ToastrService
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -28,19 +30,24 @@ export class LoginComponent {
   }
 
   login() {
-    if (this.loginForm.valid) {
-      this.http.post('http://localhost:3000/auth/login', this.loginForm.value)
-        .subscribe({
-          next: (response: any) => {
-            localStorage.setItem('token', response.token);
-            this.router.navigate(['/dashboard']);
-          },
-          error: () => {
-            this.errorMessage = 'Email ou mot de passe incorrect';
-          }
-        });
+    if (this.loginForm.invalid) {
+      this.toastr.error('Email ou mot de passe incorrect', 'Erreur');
+      return;
     }
-  }
-    
 
+    this.http.post('http://localhost:3000/auth/login', this.loginForm.value)
+      .subscribe({
+        next: (response: any) => {
+          localStorage.setItem('token', response.token);
+          this.toastr.success(
+            "Ravi de vous retrouver ! Gérez vos demandes et accédez à vos documents en toute sérénité.",
+            "Bienvenue"
+          );
+          this.router.navigate(['/dashboard']);
+        },
+        error: () => {
+          this.toastr.error('Email ou mot de passe incorrect', 'Erreur');
+        }
+      });
+  }
 }
