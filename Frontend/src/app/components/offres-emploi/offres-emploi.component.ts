223f9d289430c68baaa5c023d49f935d53570ce0
MalekSghaier <melak.seghaier@gmail.com>
2025-03-29 05:54:16 +0100

    Commi

--- Frontend/src/app/components/offres-emploi/offres-emploi.component.ts
+++ Frontend/src/app/components/offres-emploi/offres-emploi.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation,AfterViewInit, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
@@ -10,6 +10,56 @@ import { SharedNavbarComponent } from '../shared-navbar/shared-navbar.component'
   standalone: true,
   imports: [CommonModule, MatTabsModule, RouterModule,SharedSidebarComponent,SharedNavbarComponent],
   templateUrl: './offres-emploi.component.html',
-  styleUrls: ['./offres-emploi.component.css']
+  styleUrls: ['./offres-emploi.component.css'],
+  encapsulation: ViewEncapsulation.None 
+
 })
-export class OffresEmploiComponent { }
+export class OffresEmploiComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.initializeSidebar();
+ 
+  }
+  private initializeSidebar(): void {
+    const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+    allSideMenu.forEach(item => {
+      const li = item.parentElement;
+
+      if (li) {
+        item.addEventListener('click', function () {
+          allSideMenu.forEach(i => {
+            if (i.parentElement) {
+              i.parentElement.classList.remove('active');
+            }
+          });
+          li.classList.add('active');
+        });
+      }
+    });
+
+    const menuBar = document.querySelector('#content nav .bx.bx-menu');
+    const sidebar = document.getElementById('sidebar');
+
+    if (menuBar && sidebar) {
+      menuBar.addEventListener('click', function () {
+        sidebar.classList.toggle('hide');
+      });
+    }
+
+    window.addEventListener('load', this.adjustSidebar);
+    window.addEventListener('resize', this.adjustSidebar);
+  }
+
+  private adjustSidebar(): void {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar) {
+      if (window.innerWidth <= 576) {
+        sidebar.classList.add('hide');
+        sidebar.classList.remove('show');
+      } else {
+        sidebar.classList.remove('hide');
+        sidebar.classList.add('show');
+      }
+    }
+  }
+ }
