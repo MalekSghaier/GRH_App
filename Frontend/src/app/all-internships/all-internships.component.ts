97f2ace9698e4bdfc4a1a7ee714876c2151f5f92
MalekSghaier <melak.seghaier@gmail.com>
2025-04-13 19:48:23 +0100

    Commit

--- Frontend/src/app/all-internships/all-internships.component.ts
+++ Frontend/src/app/all-internships/all-internships.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { InternshipOffersService } from '../services/internship-offers.service';
 import { TruncatePipe } from '../pipes/truncate.pipe';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-all-internships',
@@ -15,10 +17,20 @@ export class AllInternshipsComponent implements OnInit {
   internshipOffers: any[] = [];
   isLoading = true;
 
-  constructor(private internshipOfferService: InternshipOffersService) {}
+  constructor(
+    private internshipOfferService: InternshipOffersService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.loadAllInternshipOffers();
+    
+    // Solution 1: Scroller vers le haut lors de la navigation
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
   }
 
   loadAllInternshipOffers() {
@@ -26,6 +38,8 @@ export class AllInternshipsComponent implements OnInit {
       next: (offers) => {
         this.internshipOffers = offers;
         this.isLoading = false;
+        // Solution alternative: Scroller après le chargement
+        setTimeout(() => window.scrollTo(0, 0), 0);
       },
       error: (err) => {
         console.error('Error loading offers:', err);
